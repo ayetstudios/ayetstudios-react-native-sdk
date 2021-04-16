a3a2c2ac1595cf63a541f3c5640b0ccd563d8885
ivanayet <ivan.marinkovic@ayetstudios.com>
2021-04-16 16:38:12 +0200

    TODO: created AyetOfferwall component

--- src/ayet_sdk.d.ts
+++ src/ayet_sdk.d.ts
@@ -4,16 +4,6 @@ declare namespace AyetSDK {
 
 
   class AyetSDK {
-    init(string: externalIdentifier, callback: userBalance): void 
-
-    getAvailableBalance(callback: getAvailableSdkBalance): void 
-
-    isInitialized(callback: wasSDKInitialised): void 
-
-    showOfferwall(string: adslotName): void 
-
-    getNativeOffers(string: adslotName, callback: getNativeOffersList): void 
-
-    activateOffer(string: offerId, callback: wasOfferActivated): void 
+    getAid(callback: getPhonesAdvertisingId): void 
   }
 }
--- src/ayet_sdk.js
+++ src/ayet_sdk.js
@@ -1,3 +1,68 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform, View, Text, Dimensions } from 'react-native';
+import React,{useState,useEffect, Component} from 'react';
+import { WebView } from 'react-native-webview';
+const { AyetSDK } = NativeModules;
 
-module.exports = NativeModules.AyetSDK;
+
+class AyetOfferwall extends Component {
+
+    constructor(props) {
+      super(props)
+      this.state = {
+        gaid: ''
+      }
+    }
+    
+     getAdvertisingID(){
+      AyetSDK.getAid((advId) => {
+        this.setState({
+          gaid: advId
+        });
+      });
+    }
+
+    componentDidMount(){
+      this.getAdvertisingID();
+    }
+
+    render() {
+      let gaid = '';
+
+      let deviceInfo ='&os_version=' + Platform.Version + '&os=' + Platform.OS + '&model=' + Platform.constants.Model + '&make=' +  Platform.constants.Manufacturer + '&type=' + ((Platform.OS == 'default') ? 'desktop' : 'phone');
+
+
+      if(Platform.OS == 'android'){
+        gaid = '&advertising_id='+this.state.gaid;
+      }else if(Platform.OS == 'ios' && this.props.idfa){
+        gaid = '&advertising_id='+this.props.idfa;
+      }else{
+        console.log('desktop ',Platform.OS);
+      }
+
+      return (
+          <WebView
+            source={{ 
+              uri: 'http://ayetstudios.com/offers/web_offerwall/' + this.props.adslotId + '?external_identifier='+this.props.userId + deviceInfo + gaid
+            }}
+            style={{ 
+              marginTop: 0,
+              marginLeft: 0,
+              marginRigh: 0,
+              marginBottom: 0,
+              minHeight: Dimensions.get('window').height
+            }}
+            onMessage={(event) => {
+              if(event.nativeEvent.data === 'goBack'){
+                this.props.onClose()
+              }
+            }}
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+            startInLoadingState={false}
+            scalesPageToFit={true}
+          />
+      );
+    }
+}
+  
+export { AyetOfferwall };
